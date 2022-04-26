c8f7dc14cbffdd3148524b89c2ac01110eddda76
Sander van 't Einde <sandervanteinde@gmail.com>
2022-04-26 21:36:46 +0200

    undo test

--- Blazor.CliTool/Program.cs
+++ Blazor.CliTool/Program.cs
@@ -9,8 +9,6 @@ var builder = CoconaApp.CreateBuilder();
 builder.Services
     .AddLogging(opts =>
     {
-
-
         if (builder.Environment.IsDevelopment())
         {
             opts.SetMinimumLevel(LogLevel.Trace);
